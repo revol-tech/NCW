278045157ef37196c99634295dd3348d4086c517
revol-tech <revolt.techno@gmail.com>
2012-05-27 09:31:27 +0545

    initial payload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NCW::Application.config.secret_token = '4e4faa8e35396676135aeb74d191be6a99807b5f23460e0809758cf6699095ed3d2254f99ac9999b6c244c5af38dff6ae0c92b9f99317a68bc3665b2b455e5b5'
