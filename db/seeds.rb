aac65a1cb76795646e6ee5a6a243724f68000811
Logic-Seeker <shobhabstha@gmail.com>
2012-05-29 21:50:29 +0545

    data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+["Laptops","Graphics Card"].each do |t|
+	product_name = t
+	Category.create!(:name=>product_name)
+end
+
+["xps-12|1|1500|public/uploads/product/image/1/image3.jpeg|true|true"].each do |t|
+	name,category_id,model_no,image,feature,offer = t.chomp.split("|")
+	Product.create!(:name=>name,:category_id=>category_id,:model_no=>model_no,:image=>File.open(image,'rb'),:feature=>feature,:offer=>offer)
+end
